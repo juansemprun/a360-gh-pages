1e2cd908ccd1ad302f6d328ded635a3d67f67d7f
Juan Semprun <juansemprun.dev@gmail.com>
2025-08-13 09:41:53 +0200

    fix: Update import paths in navbar index to use absolute paths

--- src/components/ui/navbar/index.ts
+++ src/components/ui/navbar/index.ts
@@ -1,4 +1,4 @@
-export { default as AboutUsDropdown } from './AboutUsDropdown.astro';
-export { default as FeaturesDropdown } from './FeaturesDropdown.astro';
-export { default as NavbarMobile } from './NavbarMobile.astro';
-export { default as UseCasesDropdown } from './UseCasesDropdown.astro';
+export { default as AboutUsDropdown } from '@ui/navbar/AboutUsDropdown.astro';
+export { default as FeaturesDropdown } from '@ui/navbar/FeaturesDropdown.astro';
+export { default as NavbarMobile } from '@ui/navbar/NavbarMobile.astro';
+export { default as UseCasesDropdown } from '@ui/navbar/UseCasesDropdown.astro';
