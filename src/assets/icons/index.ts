8466aa3135931f6b45c86ef8ef319f8ce0fabb4b
Juan Semprun <juansemprun.dev@gmail.com>
2025-08-13 09:40:52 +0200

    feat: Add new icons for Use Cases dropdown and update layout

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,8 @@
+export { default as TicketAirline } from '@assets/icons/ticket-airline.svg';
+export { default as Sitemap } from '@assets/icons/sitemap.svg';
+export { default as Hotel } from '@assets/icons/hotel.svg';
+export { default as Gem } from '@assets/icons/gem.svg';
+export { default as GamepadModern } from '@assets/icons/gamepad-modern.svg';
+export { default as CourtSport } from '@assets/icons/court-sport.svg';
+export { default as BuildingColumns } from '@assets/icons/building-columns.svg';
+export { default as BagsShopping } from '@assets/icons/bags-shopping.svg';
