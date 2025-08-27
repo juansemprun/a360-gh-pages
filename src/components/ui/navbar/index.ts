0ad93bda9b1678c51bd618f6dc53dbca67982fe0
Juan Semprun <juansemprun.dev@gmail.com>
2025-08-27 15:29:05 +0200

    refactor: replace gradient dropdown components with desktop variants for improved clarity and maintainability

--- src/components/ui/navbar/index.ts
+++ src/components/ui/navbar/index.ts
@@ -1,8 +1,7 @@
-export { default as AboutUsDropdown } from '@ui/navbar/AboutUsDropdown.astro';
-export { default as FeaturesDropdown } from '@ui/navbar/FeaturesDropdown.astro';
+export { default as DesktopAboutUsDropdown } from '@ui/navbar/desktop/DesktopAboutUsDropdown.astro';
+export { default as DesktopFeaturesDropdown } from '@ui/navbar/desktop/DesktopFeaturesDropdown.astro';
+export { default as DesktopUseCasesDropdown } from '@ui/navbar/desktop/DesktopUseCasesDropdown.astro';
+export { default as MobileAboutUsDropdown } from '@ui/navbar/mobile/MobileAboutUsDropdown.astro';
+export { default as MobileFeaturesDropdown } from '@ui/navbar/mobile/MobileFeaturesDropdown.astro';
+export { default as MobileUseCasesDropdown } from '@ui/navbar/mobile/MobileUseCasesDropdown.astro';
 export { default as NavbarMobile } from '@ui/navbar/NavbarMobile.astro';
-export { default as UseCasesDropdown } from '@ui/navbar/UseCasesDropdown.astro';
-
-export { default as AboutUsDropdownGradient } from '@ui/navbar/AboutUsDropdownGradient.astro';
-export { default as FeaturesDropdownGradient } from '@ui/navbar/FeaturesDropdownGradient.astro';
-export { default as UseCasesDropdownGradient } from '@ui/navbar/UseCasesDropdownGradient.astro';
