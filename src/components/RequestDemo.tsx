964660eacaee62f6f1851c18da7d884a00e546ec
Juan Semprun <juansemprun.dev@gmail.com>
2025-09-01 19:07:19 +0200

    feat: add RequestDemoComponent with calendar integration and configuration

---
+++ src/components/RequestDemo.tsx
@@ -0,0 +1,40 @@
+import Cal, { getCalApi } from '@calcom/embed-react';
+import { useEffect } from 'react';
+
+export default function RequestDemoComponent() {
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi({ namespace: '30min' });
+      cal('ui', {
+        theme: 'light',
+        hideEventTypeDetails: false,
+        layout: 'month_view',
+        cssVarsPerTheme: {
+          light: {
+            'cal-brand': '#6F61C0',
+            'cal-text': '#6F61C0',
+            'cal-text-emphasis': '#4D408D',
+            'cal-border-emphasis': '#4D408D',
+            'cal-text-error': 'pink',
+            'cal-border': '#A090E0',
+            'cal-border-default': '#A090E0',
+            'cal-border-booker': '#A090E0',
+            'cal-text-muted': '#C0B8FF',
+            'cal-bg-emphasis': '#E1DFFF',
+            'cal-border-booker-width': '3px',
+          },
+          dark: {},
+        },
+      });
+    })();
+  }, []);
+
+  return (
+    <Cal
+      namespace="30min"
+      calLink="robin-aubry-air360/30min"
+      style={{ width: '100%', height: '100%', overflow: 'scroll' }}
+      config={{ layout: 'month_view' }}
+    />
+  );
+}
