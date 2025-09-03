dc03691da3905c15683009c7a53f0b5161f41f7b
Juan Semprun <juansemprun.dev@gmail.com>
2025-09-03 11:43:29 +0200

    fix: remove trailing slash from base path in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://juansemprun.github.io',
-  base: '/a360-gh-pages/',
+  base: '/a360-gh-pages',
   build: {
     inlineStylesheets: 'always',
   },
