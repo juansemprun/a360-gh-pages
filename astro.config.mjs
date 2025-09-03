308e35bf533eb16ace78927b701b6d7d600624cf
Juan Semprun <juansemprun.dev@gmail.com>
2025-09-03 11:42:54 +0200

    fix: update base path in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://juansemprun.github.io',
+  base: '/a360-gh-pages/',
   build: {
     inlineStylesheets: 'always',
   },
