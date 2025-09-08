3d53c628da97ef8f2efb44724f5a231660137367
Juan Semprún <33623712+juansemprun@users.noreply.github.com>
2025-09-09 00:11:45 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://juansemprun.github.io',
+  base: '/a360-gh-pages/'
   build: {
     inlineStylesheets: 'always',
   },
