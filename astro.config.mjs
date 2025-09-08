15e24bba8612e2e24a8370b0957fb3e6fe081368
Juan Semprún <33623712+juansemprun@users.noreply.github.com>
2025-09-08 23:58:30 +0200

    Remove base path from Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://juansemprun.github.io',
-  base: '/a360-gh-pages',
   build: {
     inlineStylesheets: 'always',
   },
