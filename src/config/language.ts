7a95c2de6a7e1bdc775fa4b8bfa1610fe9dab193
Juan Semprun <juansemprun.dev@gmail.com>
2025-09-09 11:49:47 +0200

    feat: replace icon imports with inline flag components in LanguagePicker

--- src/config/language.ts
+++ src/config/language.ts
@@ -1,14 +1,9 @@
 import type { SvgComponentProps } from 'astro/assets/runtime';
 
-import Japan from '@assets/icons/flags/japan.svg';
-import France from '@assets/icons/flags/france.svg';
-import America from '@assets/icons/flags/america.svg';
-
 export interface Language {
   code: string; // Language code, e.g., 'en'
   label: string; // Display label, e.g., 'English'
   locale: string; // Locale string, e.g., 'en-US'
-  icon: any;
   default: boolean; // Marks the default language
 }
 
@@ -17,21 +12,18 @@ export const languages: Language[] = [
     code: 'en',
     label: 'English',
     locale: 'en-US',
-    icon: America,
     default: true,
   },
   {
     code: 'fr',
     label: 'Français',
     locale: 'fr-FR',
-    icon: France,
     default: false,
   },
   {
     code: 'ja',
     label: '日本語',
     locale: 'ja-JP',
-    icon: Japan,
     default: false,
   },
 ];
