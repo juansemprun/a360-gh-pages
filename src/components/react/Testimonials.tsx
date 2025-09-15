7e10369891a7e6bf516a8309af5d0ac2b59a528f
Juan Semprun <juansemprun.dev@gmail.com>
2025-09-15 11:50:39 +0200

    feat: add testimonials component with auto-scroll functionality
    - Introduced a new Testimonials component utilizing embla-carousel-auto-scroll for automatic scrolling.
    - Created Avatar, Button, Card, and Carousel components for UI consistency.
    - Updated package.json and pnpm-lock.yaml to include new dependencies: @radix-ui/react-avatar, @radix-ui/react-slot, embla-carousel-auto-scroll, and embla-carousel-react.
    - Integrated Testimonials component into the main index.astro page.

---
+++ src/components/react/Testimonials.tsx
@@ -0,0 +1,217 @@
+import { useRef } from 'react';
+
+import AutoScroll from 'embla-carousel-auto-scroll';
+
+import { Avatar, AvatarImage } from '@/components/ui/shadcn/avatar';
+import { Button } from '@/components/ui/shadcn/button';
+import { Card } from '@/components/ui/shadcn/card';
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/shadcn/carousel';
+
+const testimonials1 = [
+  {
+    name: 'John Doe',
+    role: 'CEO & Founder',
+    avatar: 'https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-1.webp',
+    content:
+      'Lorem ipsum dolor sit, amet Odio, incidunt. Ratione, ullam? Iusto id ut omnis repellat.',
+  },
+  {
+    name: 'John Doe',
+    role: 'CEO & Founder',
+    avatar: 'https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-1.webp',
+    content:
+      'Lorem ipsum dolor sit, amet Odio, incidunt. Ratione, ullam? Iusto id ut omnis repellat.',
+  },
+  {
+    name: 'John Doe',
+    role: 'CEO & Founder',
+    avatar: 'https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-1.webp',
+    content:
+      'Lorem ipsum dolor sit, amet Odio, incidunt. Ratione, ullam? Iusto id ut omnis repellat.',
+  },
+  {
+    name: 'Jane Doe',
+    role: 'CTO',
+    avatar: 'https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-2.webp',
+    content:
+      'Lorem ipsum dolor sit, amet Odio, incidunt. Ratione, ullam? Iusto id ut omnis repellat.',
+  },
+  {
+    name: 'John Smith',
+    role: 'COO',
+    avatar: 'https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-3.webp',
+    content:
+      'Lorem ipsum dolor sit, amet Odio, incidunt. Ratione, ullam? Iusto id ut omnis repellat.',
+  },
+  {
+    name: 'Jane Smith',
+    role: 'Tech Lead',
+    avatar: 'https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-4.webp',
+    content:
+      'Lorem ipsum dolor sit, amet Odio, incidunt. Ratione, ullam? Iusto id ut omnis repellat.',
+  },
+  {
+    name: 'Richard Doe',
+    role: 'Designer',
+    avatar: 'https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-5.webp',
+    content:
+      'Lorem ipsum dolor sit, amet Odio, incidunt. Ratione, ullam? Iusto id ut omnis repellat.',
+  },
+  {
+    name: 'Gordon Doe',
+    role: 'Developer',
+    avatar: 'https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-6.webp',
+    content:
+      'Lorem ipsum dolor sit, amet Odio, incidunt. Ratione, ullam? Iusto id ut omnis repellat.',
+  },
+];
+const testimonials2 = [
+  {
+    name: 'John Doe',
+    role: 'CEO & Founder',
+    avatar: 'https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-1.webp',
+    content:
+      'Lorem ipsum dolor sit, amet Odio, incidunt. Ratione, ullam? Iusto id ut omnis repellat.',
+  },
+  {
+    name: 'John Doe',
+    role: 'CEO & Founder',
+    avatar: 'https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-1.webp',
+    content:
+      'Lorem ipsum dolor sit, amet Odio, incidunt. Ratione, ullam? Iusto id ut omnis repellat.',
+  },
+  {
+    name: 'John Doe',
+    role: 'CEO & Founder',
+    avatar: 'https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-1.webp',
+    content:
+      'Lorem ipsum dolor sit, amet Odio, incidunt. Ratione, ullam? Iusto id ut omnis repellat.',
+  },
+  {
+    name: 'Jane Doe',
+    role: 'CTO',
+    avatar: 'https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-2.webp',
+    content:
+      'Lorem ipsum dolor sit, amet Odio, incidunt. Ratione, ullam? Iusto id ut omnis repellat.',
+  },
+  {
+    name: 'John Smith',
+    role: 'COO',
+    avatar: 'https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-3.webp',
+    content:
+      'Lorem ipsum dolor sit, amet Odio, incidunt. Ratione, ullam? Iusto id ut omnis repellat.',
+  },
+  {
+    name: 'Jane Smith',
+    role: 'Tech Lead',
+    avatar: 'https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-4.webp',
+    content:
+      'Lorem ipsum dolor sit, amet Odio, incidunt. Ratione, ullam? Iusto id ut omnis repellat.',
+  },
+  {
+    name: 'Richard Doe',
+    role: 'Designer',
+    avatar: 'https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-5.webp',
+    content:
+      'Lorem ipsum dolor sit, amet Odio, incidunt. Ratione, ullam? Iusto id ut omnis repellat.',
+  },
+  {
+    name: 'Gordon Doe',
+    role: 'Developer',
+    avatar: 'https://deifkwefumgah.cloudfront.net/shadcnblocks/block/avatar-6.webp',
+    content:
+      'Lorem ipsum dolor sit, amet Odio, incidunt. Ratione, ullam? Iusto id ut omnis repellat.',
+  },
+];
+
+const Testimonials = () => {
+  const plugin1 = useRef(
+    AutoScroll({
+      startDelay: 500,
+      speed: 0.7,
+    }),
+  );
+
+  const plugin2 = useRef(
+    AutoScroll({
+      startDelay: 500,
+      speed: 0.7,
+      direction: 'backward',
+    }),
+  );
+  return (
+    <section className="pb-32" data-theme="dark">
+      <div className="container-custom flex flex-col items-center gap-6">
+        <h2 className="font-clash-display-bold mb-2 text-center text-2xl lg:text-5xl">
+          <span className="block">What changed for the better</span>
+          <span className="block">
+            since you switched to <mark className="bg-primary-500 p-1 text-white">Air360</mark>?
+          </span>
+        </h2>
+        <p className="text-muted-foreground lg:text-lg">
+          Discover how <strong className="text-primary-500 font-bold">Air360</strong> is
+          transforming workflows across industries
+        </p>
+        <Button className="mt-6">Get started for free</Button>
+      </div>
+      <div>
+        <div className="mt-16 space-y-4">
+          <Carousel
+            opts={{
+              loop: true,
+            }}
+            plugins={[plugin1.current]}
+            onMouseLeave={() => plugin1.current.play()}
+          >
+            <CarouselContent>
+              {testimonials1.map((testimonial, index) => (
+                <CarouselItem key={index} className="basis-auto">
+                  <Card className="max-w-96 select-none p-6">
+                    <div className="mb-4 flex gap-4">
+                      <Avatar className="ring-input size-9 rounded-full ring-1">
+                        <AvatarImage src={testimonial.avatar} alt={testimonial.name} />
+                      </Avatar>
+                      <div className="text-sm">
+                        <p className="font-medium">{testimonial.name}</p>
+                        <p className="text-muted-foreground">{testimonial.role}</p>
+                      </div>
+                    </div>
+                    <q>{testimonial.content}</q>
+                  </Card>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
+          <Carousel
+            opts={{
+              loop: true,
+            }}
+            plugins={[plugin2.current]}
+            onMouseLeave={() => plugin2.current.play()}
+          >
+            <CarouselContent>
+              {testimonials2.map((testimonial, index) => (
+                <CarouselItem key={index} className="basis-auto">
+                  <Card className="max-w-96 select-none p-6">
+                    <div className="mb-4 flex gap-4">
+                      <Avatar className="ring-input size-9 rounded-full ring-1">
+                        <AvatarImage src={testimonial.avatar} alt={testimonial.name} />
+                      </Avatar>
+                      <div className="text-sm">
+                        <p className="font-medium">{testimonial.name}</p>
+                        <p className="text-muted-foreground">{testimonial.role}</p>
+                      </div>
+                    </div>
+                    <q>{testimonial.content}</q>
+                  </Card>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export { Testimonials };
