91025a8f7df1da1c648abc23b6593be6fab1303e
Juan Semprun <juansemprun.dev@gmail.com>
2025-09-17 00:14:50 +0200

    feat: enhance Blog component with new layout, badges, and improved article previews

--- src/components/react/Blog.tsx
+++ src/components/react/Blog.tsx
@@ -1,134 +1,321 @@
-import { FileText } from 'lucide-react';
+import { ArrowRight, FileText, Calendar, ChevronRight } from 'lucide-react';
 
-import { Separator } from '@/components/ui/shadcn/separator';
 import { Avatar, AvatarImage } from '@/components/ui/shadcn/avatar';
 import { Badge } from '@/components/ui/shadcn/badge';
-
-const blogs = [
-  {
-    title: 'How to design a website from scratch',
-    image:
-      'https://www.air360.io/blog/conversion-rate-optimization/nextImageExportOptimizer/hero-opt-1080.WEBP',
-    date: 'May 20, 2024',
-    tag: 'Marketing',
-  },
-  {
-    title: 'The best tools for web development and design',
-    image: 'https://www.air360.io/blog/data-processing/nextImageExportOptimizer/1-opt-1080.WEBP',
-    date: 'June 12, 2024',
-    tag: 'UX',
-  },
-  {
-    title: 'How to market your website and get more traffic',
-    image: 'https://www.air360.io/blog/what-is-utm/nextImageExportOptimizer/1-opt-1080.WEBP',
-    date: 'July 5, 2024',
-    tag: 'E-Commerce',
-  },
-  {
-    title: 'The future of web development and design',
-    image:
-      'https://www.air360.io/blog/how_to_effectively_use_heatmaps/nextImageExportOptimizer/1-opt-1920.WEBP',
-    date: 'August 18, 2024',
-    tag: 'Data',
-  },
-  {
-    title: 'How to design a website from scratch',
-    image:
-      'https://www.air360.io/blog/conversion-rate-optimization/nextImageExportOptimizer/hero-opt-1080.WEBP',
-    date: 'May 20, 2024',
-    tag: 'Marketing',
-  },
-  {
-    title: 'The best tools for web development and design',
-    image: 'https://www.air360.io/blog/data-processing/nextImageExportOptimizer/1-opt-1080.WEBP',
-    date: 'June 12, 2024',
-    tag: 'UX',
-  },
-  {
-    title: 'How to market your website and get more traffic',
-    image: 'https://www.air360.io/blog/what-is-utm/nextImageExportOptimizer/1-opt-1080.WEBP',
-    date: 'July 5, 2024',
-    tag: 'E-Commerce',
-  },
-  {
-    title: 'The future of web development and design',
-    image:
-      'https://images.unsplash.com/photo-1546414701-81cc6963c67f?q=80&w=2144&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    date: 'August 18, 2024',
-    tag: 'Data',
-  },
-];
+import { Button } from '@/components/ui/shadcn/button';
+import { Separator } from '@/components/ui/shadcn/separator';
 
 const Blog = () => {
   return (
-    <section className="bg-muted/60 py-32" data-theme="dark">
-      <div className="container-custom">
-        <div className="relative mx-auto flex max-w-7xl flex-col gap-20 lg:flex-row">
-          <header className="top-10 flex h-fit flex-col items-center gap-5 text-center lg:sticky lg:max-w-80 lg:items-start lg:gap-8 lg:text-left">
-            <FileText className="invisible h-5 w-14" strokeWidth={1} />
-            <h1 className="font-clash-display-bold text-4xl lg:text-5xl">Blog Posts</h1>
-            <p className="text-muted-foreground lg:text-xl">
-              Our experts share their knowledge to help you build better applications.
-            </p>
-            <Separator />
-            <nav>
-              <ul className="flex flex-wrap items-center justify-center gap-4 lg:flex-col lg:items-start lg:gap-2">
-                <li className="font-medium">
-                  <a href="#">All</a>
-                </li>
-                <li className="text-muted-foreground hover:text-primary">
-                  <a href="#">Design</a>
-                </li>
-                <li className="text-muted-foreground hover:text-primary">
-                  <a href="#">Development</a>
-                </li>
-                <li className="text-muted-foreground hover:text-primary">
-                  <a href="#">Marketing</a>
-                </li>
-              </ul>
-            </nav>
-          </header>
-          <div className="grid gap-4 md:grid-cols-2">
-            {blogs.map(({ title, date, image, tag }, index) => (
-              <a className="rounded-xl border" href="#" key={index}>
-                <div className="relative p-2">
-                  <img
-                    src={image}
-                    alt="placeholder"
-                    className="aspect-video w-full rounded-lg object-cover"
-                  />
-                  <Badge
-                    variant="secondary"
-                    className="bg-background/70 absolute right-4 top-4 px-3 py-1 text-sm backdrop-blur-sm"
-                  >
-                    {tag}
-                  </Badge>
-                </div>
-                <div className="px-3 pb-4 pt-2">
-                  <h2 className="mb-1 font-medium">{title}</h2>
-                  <p className="text-muted-foreground line-clamp-2 text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua.
-                  </p>
-                  <Separator className="my-5" />
-                  <div className="flex items-center justify-between gap-4">
-                    <div className="flex items-center gap-3">
-                      <Avatar className="ring-input size-9 rounded-full">
-                        <AvatarImage
-                          src="https://www.air360.io/nextImageExportOptimizer/favicon2-opt-96.WEBP"
-                          className="object-contain"
-                          alt="placeholder"
-                        />
-                      </Avatar>
-                      <span className="text-sm font-medium">Air360 Team</span>
-                    </div>
-                    <Badge variant="secondary" className="h-fit">
-                      10 Min Read
-                    </Badge>
-                  </div>
-                </div>
+    <section className="container-custom py-32" data-theme="dark">
+      <div className="mx-auto flex max-w-3xl flex-col items-center gap-4 text-center">
+        <h1 className="font-clash-display-bold text-balance text-4xl md:text-6xl">Air360 Blog</h1>
+        <p className="text-muted-foreground text-sm md:text-xl">
+          Explore our blog for insightful articles, personal reflections and ideas that inspire
+          action on the topics you care about.
+        </p>
+        <nav>
+          <ul className="flex flex-wrap items-center justify-center gap-4 lg:gap-2">
+            <li className="font-medium">
+              <a href="#">
+                <Badge className="bg-primary-500 h-fit px-3 py-1">All</Badge>
               </a>
-            ))}
+            </li>
+            <li className="text-muted-foreground hover:text-primary">
+              <a href="#">
+                <Badge variant="outline" className="h-fit px-3 py-1">
+                  Design
+                </Badge>
+              </a>
+            </li>
+            <li className="text-muted-foreground hover:text-primary">
+              <a href="#">
+                <Badge variant="outline" className="h-fit px-3 py-1">
+                  Development
+                </Badge>
+              </a>
+            </li>
+            <li className="text-muted-foreground hover:text-primary">
+              <a href="#">
+                <Badge variant="outline" className="h-fit px-3 py-1">
+                  Marketing
+                </Badge>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="mt-20 grid gap-10 md:grid-cols-2">
+        <a className="rounded-xl border" href="#">
+          <div className="relative p-2">
+            <img
+              src="https://images.unsplash.com/photo-1536735561749-fc87494598cb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NDI3NzN8MHwxfGFsbHwxNzd8fHx8fHwyfHwxNzIzNjM0NDc0fA&ixlib=rb-4.0.3&q=80&w=1080"
+              alt="placeholder"
+              className="aspect-video w-full rounded-lg object-cover"
+            />
+            <Badge
+              variant="secondary"
+              className="bg-background/70 absolute right-4 top-4 px-3 py-1 text-sm backdrop-blur-sm"
+            >
+              Marketing
+            </Badge>
+          </div>
+          <div className="px-3 pb-4 pt-2">
+            <h2 className="mb-1 font-medium">
+              How to build a successful brand and business online in 2024
+            </h2>
+            <p className="text-muted-foreground line-clamp-2 text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua.
+            </p>
+            <Separator className="my-5" />
+            <div className="flex items-center justify-between gap-4">
+              <div className="flex items-center gap-3">
+                <Avatar className="ring-input size-9 rounded-full">
+                  <AvatarImage
+                    src="https://www.air360.io/nextImageExportOptimizer/favicon2-opt-96.WEBP"
+                    className="object-contain"
+                    alt="placeholder"
+                  />
+                </Avatar>
+                <span className="text-sm font-medium">John Doe</span>
+              </div>
+              <Badge variant="secondary" className="h-fit">
+                10 Min Read
+              </Badge>
+            </div>
+          </div>
+        </a>
+        <a className="rounded-xl border" href="#">
+          <div className="relative p-2">
+            <img
+              src="https://images.unsplash.com/photo-1653288973812-81d1951b8127?q=80&w=2022&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="placeholder"
+              className="aspect-video w-full rounded-lg object-cover"
+            />
+            <Badge
+              variant="secondary"
+              className="bg-background/70 absolute right-4 top-4 px-3 py-1 text-sm backdrop-blur-sm"
+            >
+              E-Commerce
+            </Badge>
+          </div>
+          <div className="px-3 pb-4 pt-2">
+            <h2 className="mb-1 font-medium">
+              The difference between UI and UX and how to design for both
+            </h2>
+            <p className="text-muted-foreground line-clamp-2 text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua.
+            </p>
+            <Separator className="my-5" />
+            <div className="flex items-center justify-between gap-4">
+              <div className="flex items-center gap-3">
+                <Avatar className="ring-input size-9 rounded-full">
+                  <AvatarImage
+                    src="https://www.air360.io/nextImageExportOptimizer/favicon2-opt-96.WEBP"
+                    className="object-contain"
+                    alt="placeholder"
+                  />
+                </Avatar>
+                <span className="text-sm font-medium">Jane Doe</span>
+              </div>
+              <Badge variant="secondary" className="h-fit">
+                14 Min Read
+              </Badge>
+            </div>
+          </div>
+        </a>
+        <a className="rounded-xl border" href="#">
+          <div className="relative p-2">
+            <img
+              src="https://images.unsplash.com/photo-1563952532949-3d1a874ad614?q=80&w=1951&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="placeholder"
+              className="aspect-video w-full rounded-lg object-cover"
+            />
+            <Badge
+              variant="secondary"
+              className="bg-background/70 absolute right-4 top-4 px-3 py-1 text-sm backdrop-blur-sm"
+            >
+              UX
+            </Badge>
+          </div>
+          <div className="px-3 pb-4 pt-2">
+            <h2 className="mb-1 font-medium">
+              Optimizing your website for SEO and getting more traffic
+            </h2>
+            <p className="text-muted-foreground line-clamp-2 text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua.
+            </p>
+            <Separator className="my-5" />
+            <div className="flex items-center justify-between gap-4">
+              <div className="flex items-center gap-3">
+                <Avatar className="ring-input size-9 rounded-full">
+                  <AvatarImage
+                    src="https://www.air360.io/nextImageExportOptimizer/favicon2-opt-96.WEBP"
+                    className="object-contain"
+                    alt="placeholder"
+                  />
+                </Avatar>
+                <span className="text-sm font-medium">Jane Smith</span>
+              </div>
+              <Badge variant="secondary" className="h-fit">
+                9 Min Read
+              </Badge>
+            </div>
+          </div>
+        </a>
+        <a className="rounded-xl border" href="#">
+          <div className="relative p-2">
+            <img
+              src="https://images.unsplash.com/photo-1563952532949-3d1a874ad614?q=80&w=1951&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="placeholder"
+              className="aspect-video w-full rounded-lg object-cover"
+            />
+            <Badge
+              variant="secondary"
+              className="bg-background/70 absolute right-4 top-4 px-3 py-1 text-sm backdrop-blur-sm"
+            >
+              UX
+            </Badge>
+          </div>
+          <div className="px-3 pb-4 pt-2">
+            <h2 className="mb-1 font-medium">
+              Optimizing your website for SEO and getting more traffic
+            </h2>
+            <p className="text-muted-foreground line-clamp-2 text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua.
+            </p>
+            <Separator className="my-5" />
+            <div className="flex items-center justify-between gap-4">
+              <div className="flex items-center gap-3">
+                <Avatar className="ring-input size-9 rounded-full">
+                  <AvatarImage
+                    src="https://www.air360.io/nextImageExportOptimizer/favicon2-opt-96.WEBP"
+                    className="object-contain"
+                    alt="placeholder"
+                  />
+                </Avatar>
+                <span className="text-sm font-medium">Jane Smith</span>
+              </div>
+              <Badge variant="secondary" className="h-fit">
+                9 Min Read
+              </Badge>
+            </div>
+          </div>
+        </a>
+        <div className="flex flex-col">
+          <div className="relative">
+            <img
+              src="https://images.unsplash.com/photo-1653288973812-81d1951b8127?q=80&w=2022&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="placeholder"
+              className="aspect-video w-full rounded-lg object-cover"
+            />
+            <Badge
+              variant="secondary"
+              className="bg-background/70 absolute right-4 top-4 px-3 py-1 text-sm backdrop-blur-sm"
+            >
+              Design
+            </Badge>
+          </div>
+          <div className="flex h-full flex-col justify-between p-4">
+            <h2 className="mb-5 text-xl font-semibold">The difference between UI and UX</h2>
+            <div className="flex justify-between gap-6 text-sm">
+              <span className="text-muted-foreground flex items-center gap-1">
+                <Calendar className="h-4 w-4" />
+                April, 07, 2024
+              </span>
+              <a href="#" className="flex items-center gap-1">
+                Read more
+                <ChevronRight className="h-full w-3" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <div className="relative">
+            <img
+              src="https://images.unsplash.com/photo-1653288973812-81d1951b8127?q=80&w=2022&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="placeholder"
+              className="aspect-video w-full rounded-lg object-cover"
+            />
+            <Badge
+              variant="secondary"
+              className="bg-background/70 absolute right-4 top-4 px-3 py-1 text-sm backdrop-blur-sm"
+            >
+              Design
+            </Badge>
+          </div>
+          <div className="flex h-full flex-col justify-between p-4">
+            <h2 className="mb-5 text-xl font-semibold">The difference between UI and UX</h2>
+            <div className="flex justify-between gap-6 text-sm">
+              <span className="text-muted-foreground flex items-center gap-1">
+                <Calendar className="h-4 w-4" />
+                April, 07, 2024
+              </span>
+              <a href="#" className="flex items-center gap-1">
+                Read more
+                <ChevronRight className="h-full w-3" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <div className="relative">
+            <img
+              src="https://images.unsplash.com/photo-1653288973812-81d1951b8127?q=80&w=2022&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="placeholder"
+              className="aspect-video w-full rounded-lg object-cover"
+            />
+            <Badge
+              variant="secondary"
+              className="bg-background/70 absolute right-4 top-4 px-3 py-1 text-sm backdrop-blur-sm"
+            >
+              Design
+            </Badge>
+          </div>
+          <div className="flex h-full flex-col justify-between p-4">
+            <h2 className="mb-5 text-xl font-semibold">The difference between UI and UX</h2>
+            <div className="flex justify-between gap-6 text-sm">
+              <span className="text-muted-foreground flex items-center gap-1">
+                <Calendar className="h-4 w-4" />
+                April, 07, 2024
+              </span>
+              <a href="#" className="flex items-center gap-1">
+                Read more
+                <ChevronRight className="h-full w-3" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <div className="relative">
+            <img
+              src="https://images.unsplash.com/photo-1653288973812-81d1951b8127?q=80&w=2022&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="placeholder"
+              className="aspect-video w-full rounded-lg object-cover"
+            />
+            <Badge
+              variant="secondary"
+              className="bg-background/70 absolute right-4 top-4 px-3 py-1 text-sm backdrop-blur-sm"
+            >
+              Design
+            </Badge>
+          </div>
+          <div className="flex h-full flex-col justify-between p-4">
+            <h2 className="mb-5 text-xl font-semibold">The difference between UI and UX</h2>
+            <div className="flex justify-between gap-6 text-sm">
+              <span className="text-muted-foreground flex items-center gap-1">
+                <Calendar className="h-4 w-4" />
+                April, 07, 2024
+              </span>
+              <a href="#" className="flex items-center gap-1">
+                Read more
+                <ChevronRight className="h-full w-3" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
