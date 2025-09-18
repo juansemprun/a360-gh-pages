489acbf3ff0914e2774007a6ecce4a7fbea64261
Juan Semprun <juansemprun.dev@gmail.com>
2025-09-18 11:10:01 +0200

    Add VeryChic logo and update Marquee component for color themes
    - Added new SVG logo for VeryChic.
    - Updated brand-logos index to include color variants for existing logos.
    - Refactored Marquee component to dynamically render logos based on light/dark themes.
    - Adjusted styles for improved marquee animation and layout.
    - Set Marquee component to use light theme in HeroFooter.

--- src/assets/brand-logos/index.ts
+++ src/assets/brand-logos/index.ts
@@ -1,5 +1,5 @@
 export { default as AirCaraibes } from '@assets/brand-logos/air-caraibes.svg';
-export { default as AirCaraibesColor } from '@assets/brand-logos/air-caraibes-color.svg';
+// export { default as AirCaraibesColor } from '@assets/brand-logos/air-caraibes-color.svg';
 export { default as ClubMed } from '@assets/brand-logos/club-med.svg';
 export { default as Dubreuil } from '@assets/brand-logos/dubreuil.svg';
 export { default as Empruntis } from '@assets/brand-logos/empruntis.svg';
@@ -10,3 +10,16 @@ export { default as Product } from '@assets/brand-logos/product.svg';
 export { default as PsaBank } from '@assets/brand-logos/psa-bank.svg';
 export { default as Stellantis } from '@assets/brand-logos/stellantis.svg';
 export { default as VeryChic } from '@assets/brand-logos/verychic.svg';
+
+// Color
+export { default as AirCaraibesColor } from '@assets/brand-logos/color/air-caraibes.svg';
+export { default as ClubMedColor } from '@assets/brand-logos/color/club-med.svg';
+export { default as DubreuilColor } from '@assets/brand-logos/color/dubreuil.svg';
+export { default as EmpruntisColor } from '@assets/brand-logos/color/empruntis.svg';
+export { default as MaVilleMonShoppingColor } from '@assets/brand-logos/color/ma-ville-mon-shopping.svg';
+export { default as OlympiqueLyonnaisColor } from '@assets/brand-logos/color/olympique-lyonnais.svg';
+export { default as PlaybacColor } from '@assets/brand-logos/color/playbac.svg';
+export { default as ProductColor } from '@assets/brand-logos/color/product.svg';
+export { default as PsaBankColor } from '@assets/brand-logos/color/psa-bank.svg';
+export { default as StellantisColor } from '@assets/brand-logos/color/stellantis.svg';
+export { default as VeryChicColor } from '@assets/brand-logos/color/verychic.svg';
