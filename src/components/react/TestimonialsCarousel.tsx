36583fcfb3de001d18921aa6da880302d5ab2723
Juan Semprun <juansemprun.dev@gmail.com>
2025-10-27 12:18:09 +0100

    fix: adjust styling for Testimonials section and update Marquee duration to 100 seconds

--- src/components/react/TestimonialsCarousel.tsx
+++ src/components/react/TestimonialsCarousel.tsx
@@ -25,21 +25,21 @@ const ReviewCard = ({
   body: string;
 }) => {
   return (
-    <figure className="relative h-full max-w-96 cursor-pointer overflow-hidden rounded-xl border border-gray-950/[.1] bg-gray-950/[.01] p-6 hover:bg-gray-950/[.05]">
+    <figure className="relative h-full w-fit max-w-xl cursor-pointer overflow-hidden rounded-xl border border-gray-950/[.1] bg-gray-950/[.01] p-6 hover:bg-gray-950/[.05]">
       <div className="flex flex-row items-center gap-2">
-        <img
+        {/* <img
           className="rounded-full"
           width="32"
           height="32"
           alt={`${name}'s review of Air360`}
           src={img}
-        />
+        /> */}
         <div className="flex flex-col">
           <figcaption className="text-sm font-medium dark:text-white">{name}</figcaption>
           <span className="text-xs text-gray-500">{client}</span>
         </div>
       </div>
-      <blockquote className="mt-4 line-clamp-4 text-sm">{body}</blockquote>
+      <blockquote className="mt-4 text-sm">{body}</blockquote>
     </figure>
   );
 };
@@ -60,14 +60,14 @@ export function TestimonialsCarousel({
   return (
     <div className="relative mt-16 flex w-full flex-col items-center justify-center gap-4 overflow-hidden">
       {/* First Marquee - Scrolls Right */}
-      <Marquee pauseOnHover className="[--duration:80s]">
+      <Marquee pauseOnHover className="[--duration:100s]">
         {firstRow.map((review) => (
           <ReviewCard key={`carousel1-${review.id}`} {...review} />
         ))}
       </Marquee>
 
       {/* Second Marquee - Scrolls Left */}
-      <Marquee reverse pauseOnHover className="[--duration:80s]">
+      <Marquee reverse pauseOnHover className="[--duration:100s]">
         {secondRow.map((review) => (
           <ReviewCard key={`carousel2-${review.id}`} {...review} />
         ))}
