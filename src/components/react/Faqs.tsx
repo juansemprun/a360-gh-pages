e53a79e9e5daa6dbc5019752693c18ec15627664
Juan Semprun <juansemprun.dev@gmail.com>
2025-12-05 21:45:33 +0100

    Refactor FAQSection and Faqs components for improved structure and styling

--- src/components/react/Faqs.tsx
+++ src/components/react/Faqs.tsx
@@ -25,12 +25,14 @@ export const Faqs = ({ faqs }: FeatureFAQProps) => {
         <AccordionItem
           value={index.toString()}
           key={index}
-          className="bg-muted m-0 w-full rounded-xl px-4 py-2"
+          className="m-0 w-full rounded-xl px-4 py-2 shadow-sm ring ring-black/20"
         >
-          <AccordionTrigger className="flex flex-1 justify-between text-left font-semibold transition-all hover:cursor-pointer hover:no-underline">
+          <AccordionTrigger className="flex flex-1 justify-between p-2 text-left text-xl font-semibold transition-all hover:cursor-pointer hover:no-underline md:text-2xl">
             {item.question}
           </AccordionTrigger>
-          <AccordionContent className="text-muted-foreground mt-2">{item.answer}</AccordionContent>
+          <AccordionContent className="text-md mt-2 text-neutral-600 md:text-xl">
+            {item.answer}
+          </AccordionContent>
         </AccordionItem>
       ))}
     </Accordion>
