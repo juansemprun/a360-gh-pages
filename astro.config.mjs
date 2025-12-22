96cdb1300692d74050d41e89b98ffa285c261c10
Juan Semprun <juansemprun.dev@gmail.com>
2025-12-22 14:18:03 +0100

    Set trailingSlash option to 'never' in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ import { languageCodes } from './src/config/language';
 export default defineConfig({
   site: 'https://juansemprun.dev',
   // base: '/a360-gh-pages',
+  trailingSlash: 'never',
   build: {
     inlineStylesheets: 'always',
   },
